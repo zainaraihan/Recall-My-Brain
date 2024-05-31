608cb6e227a948afe460a73cc01ee2ab83ddf8fd
zainarhn <zainaraihan@gmail.com>
2024-05-31 11:13:14 +0700

    melengkapi bagian function

--- Function/function.js
+++ Function/function.js
@@ -78,12 +78,125 @@ console.log(filter([2,3,4,1,5,2], isEven))
 // -------------------------------
 
 // Callback Function
+// Dalam Javascript, callback function adalah function yang digunakan sebagai argumen di dalam function lain dan dieksekusi ketika beberapa operasi telah di selesaikan. Callback - Callback biasanya digunakan untuk menghandle Operasi Asynchronous seperti network request, file I/O, timers dan events.
 function fetchData(callback){
     setTimeout(()=>{
         callback("Data Fetched!")
-    }, 2000);
+    }, 1000);
 }
 
 fetchData((messege)=>{
     console.log(messege)
+})
+
+// // Simple Callback Function
+function doSomething(callback){
+    console.log('Doing Something . . .');
+    callback();
+}
+
+function Something(){
+    console.log('Done!');
+}
+
+doSomething(Something);
+
+// // Asynchronous Callback dengan setTimeout
+function fetchData2(callback){
+    setTimeout(()=> {
+        const data = 'Data Selesai di Fetch!';
+        callback(data);
+    }, 1000);
+}
+
+function handleData(data){
+    console.log(data);
+}
+
+fetchData2(handleData);
+
+// // Error First Callback
+function doTask(callback){
+    const error = false;
+    const result = "Task Completed";
+
+    setTimeout(() => {
+        if(error){
+            callback("An Error Occured", null);
+        }else{
+            callback(null, result);
+        }
+    }, 1000)
+}
+
+doTask(function(err, result){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(result)
+    }
+})
+
+// // Nested Callback (Callback Hell)
+function firstTask(callback){
+    setTimeout(()=> {
+        console.log('First Data Completed');
+        callback();
+    }, 1000)
+}
+function secondTask(callback){
+    setTimeout(()=> {
+        console.log('Second Data Completed');
+        callback();
+    },1000)
+}
+function thirdTask(callback){
+    setTimeout(()=> {
+        console.log('Third Data Completed');
+        callback();
+    }, 1000)
+}
+
+firstTask(function(){
+    secondTask(function(){
+        thirdTask(function(){
+            console.log('All Tasks Completed')
+        })
+    })
+})
+
+// Menghindari Callback Hell dengan Promise
+
+function taskPertama(){
+    return new Promise((resolve) => {
+        setTimeout(()=> {
+            console.log("Task Pertama Selesai");
+            resolve();
+        }, 1000)
+    })
+}
+
+function taskKedua(){
+    return new Promise((resolve)=> {
+        setTimeout(()=>{
+            console.log("Task Kedua Selesai");
+            resolve();
+        },1000)
+    })
+}
+
+function taskKetiga(){
+    return new Promise((resolve)=> {
+        setTimeout(()=>{
+            console.log("Task Ketiga Selesai");
+            resolve();
+        },1000)
+    })
+}
+
+taskPertama()
+        .then(taskKedua)
+        .then(taskKetiga)
+        .then(() => {
+        console.log('Semua task telah selesai')
 })
