17dc200dda355e874f6d035a8cc7d9d2feed62ef
zainarhn <zainaraihan@gmail.com>
2024-06-04 00:00:58 +0700

    menambah study case lain

--- Function/Study Case/case.js
+++ Function/Study Case/case.js
@@ -11,4 +11,25 @@ function averageValue(grades){
 }
 
 const a = [45,34,23,45,90,20];
-console.log(averageValue(a))
+console.log(averageValue(a));
+
+// Memisahkan Nilai Ganjil dan Genap
+function separateEvenOdd(array){
+    let even = [];
+    let odd = [];
+
+    for(arr of array){
+        if (arr % 2 === 0){
+            even.push(arr);
+        }else{
+            odd.push(arr);
+        }
+    }
+
+    return {even, odd};
+}
+
+const array = [1,2,3,4,5,90,82,38,43]
+const {even, odd} = separateEvenOdd(array);
+console.log("Angka Genap :" + even);
+console.log("Angka Ganjil :" + odd)
