d290bbb0a52cf89dcb933be17abc9feb68dd772b
zainarhn <zainaraihan@gmail.com>
2024-06-04 22:58:00 +0700

    still error on grade

---
+++ Function/Study Case/typescript/case.js
@@ -0,0 +1,15 @@
+// Menghitung rata rata nilai
+function averageValue(grades) {
+    if (grades == 0) {
+        return 0;
+    }
+    ;
+    var total = 0;
+    for (var _i = 0, grades_1 = grades; _i < grades_1.length; _i++) {
+        var grade = grades_1[_i];
+        total += grade;
+    }
+    return total / grades.length;
+}
+var vl = new Array(3, 5, 6, 2, 4, 6);
+console.log(averageValue(vl));
---
+++ Function/Study Case/typescript/case.ts
@@ -0,0 +1,17 @@
+// Menghitung rata rata nilai
+function averageValue(grades: number[]) {
+    if (grades == 0) {
+        return 0;
+    };
+
+
+    let total: number = 0;
+    for (let grade of grades) {
+        total += grade
+    }
+
+    return total / grades.length;
+}
+
+const vl: number[] = new Array(3, 5, 6, 2, 4, 6);
+console.log(averageValue(vl))
